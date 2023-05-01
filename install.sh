d018a1d00807fec8329f56c87e48f7c0189d9b3a
shield61 <hibachi.dioxin_0k@icloud.com>
2023-05-01 15:12:40 +0200

    Parsing install command

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+printf "%s " "$(tail -n -1 "$HOME/.bash_history")"
+printf "\n"
+_SOURCEURL=""
+_SOURCEBRANCH=""
+_SOURCEREPO=""
+_SOURCESCRIPT=""
+
+function getInstallCommand() {
+    OIFS=$IFS
+    IN="$(tail -n -1 "$HOME/.bash_history")"
+    #IN="curl -sSL https://raw.githubusercontent.com/shield61/little-backup-box/development/install-little-backup-box.sh | bash  2> install-error.log"
+    IFS=' ' read -r -a CMDLINE <<< "$IN"
+    for x in "${CMDLINE[@]}"
+    do
+        if [[ "$x" = *"github"* ]] ; then
+            _SOURCEURL=$x
+            IFS='/' read -r -a URL <<< "$x"
+            _SOURCESCRIPT=${URL[-1]}
+            _SOURCEBRANCH=${URL[-2]}
+            _SOURCEREPO=${URL[-3]}
+        fi
+        continue
+    done
+    IFS=$OIFS
+}
+
+if ! [[ $SHELL =~ "bash" ]] ; then
+    echo "Calling shell must be bash. Using '$SHELL' is not supported. Terminating!"
+    exit 255
+fi
+getInstallCommand
+
+echo "URL:" "${_SOURCEURL}"
+echo "SCRIPTNAME:" "${_SOURCESCRIPT}"
+echo "BRANCH:" "${_SOURCEBRANCH}"
+echo "REPOSITORY:" "${_SOURCEREPO}"
